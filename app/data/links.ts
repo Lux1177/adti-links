9a997067a39de79bb6dcfac88ec2babbedfdd15f
lux1177 <ualimdjanov10@gmail.com>
2025-10-10 15:47:21 +0500

    added new links

--- app/data/links.ts
+++ app/data/links.ts
@@ -3,6 +3,18 @@ import type Link from '~/types/link'
 export const LINKS: Link[] = [
 
 	{
+		title: 'adti.uz',
+		description: "Ideal rezyumesini yaratmoqchi bo'lgan talaba. Kelajakni birga yaratamiz!",
+		url: 'https://adti.uz/',
+		image: '/adti.uz.png'
+	},
+	{
+		title: '@asmimed',
+		description: "Talabalar to'lov, ijara va shunga o'xshash shartnomalarga ariza berish uchun",
+		url: 'https://t.me/asmimed',
+		image: '/tg.png'
+	},
+	{
 		title: 'careercentre.uz',
 		description: "Ideal rezyumesini yaratmoqchi bo'lgan talaba. Kelajakni birga yaratamiz!",
 		url: 'https://careercentre.uz/',
@@ -15,28 +27,16 @@ export const LINKS: Link[] = [
 		image: '/kontrakt.edu.uz.png'
 	},
 	{
-		title: 'careercentre.uz',
+		title: 'edu.uz',
 		description: "Ideal rezyumesini yaratmoqchi bo'lgan talaba. Kelajakni birga yaratamiz!",
-		url: 'https://careercentre.uz/',
-		image: '/careercentre.uz.png'
+		url: 'https://gov.uz/oz/edu',
+		image: '/edu.uz.png'
 	},
 	{
-		title: 'kontrakt.edu.uz',
+		title: 'ssv.uz',
 		description: "Talabalar to'lov, ijara va shunga o'xshash shartnomalarga ariza berish uchun",
-		url: 'https://kontrakt.edu.uz/',
-		image: '/kontrakt.edu.uz.png'
-	},
-	{
-		title: 'careercentre.uz',
-		description: "Ideal rezyumesini yaratmoqchi bo'lgan talaba. Kelajakni birga yaratamiz!",
-		url: 'https://careercentre.uz/',
-		image: '/careercentre.uz.png'
-	},
-	{
-		title: 'kontrakt.edu.uz',
-		description: "Talabalar to'lov, ijara va shunga o'xshash shartnomalarga ariza berish uchun",
-		url: 'https://kontrakt.edu.uz/',
-		image: '/kontrakt.edu.uz.png'
+		url: 'https://gov.uz/oz/ssv',
+		image: '/ssv.uz.png'
 	},
 	{
 		title: 'careercentre.uz',
