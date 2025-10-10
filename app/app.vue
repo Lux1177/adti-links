0e99c9b0034022a41bdbb60557149e4d5c36276a
lux1177 <ualimdjanov10@gmail.com>
2025-10-10 15:14:25 +0500

    Fixed seo and meta

--- app/app.vue
+++ app/app.vue
@@ -6,12 +6,4 @@
 
 <script setup lang="ts">
 
-useSeoMeta({
-	title: 'Link in Bio',
-	description: 'My personal link in bio page',
-	ogTitle: 'Link in Bio',
-	ogDescription: 'My personal link in bio page',
-	ogImage: '/placeholder.png',
-	twitterCard: 'summary_large_image',
-});
 </script>
